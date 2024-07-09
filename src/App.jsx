c7aad9844e59dd551f7ca8b4dbc2f7c5fa9254f8
Daniel Fraga <dan.fraga33@gmail.com>
2024-07-09 14:34:42 +0100

    feat: add sentry monitoring

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import * as Sentry from "@sentry/react";
+
 const App = () => {
+  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
   return (
     <main>
       <Navbar />
@@ -13,4 +17,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+//...
+import * as Sentry from "@sentry/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: "https://f4744547bd9a6464c99f0f341d9f19b6@o4507571731365888.ingest.de.sentry.io/4507571736674384",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
